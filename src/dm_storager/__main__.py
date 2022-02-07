ddd93330acd31a82b36bf44c2bbab1d95f7d745b
Emil Bakhabiev <e.bakhabiev@thirdpin.io>
2022-02-08 00:05:18 +0300

    [main] Test client

--- src/dm_storager/__main__.py
+++ src/dm_storager/__main__.py
@@ -14,13 +14,15 @@ if __name__ == "__main__":
     moro_big_scanner = ScannerStat(address="192.168.1.10", port=5001, scanner_id=0x02)
 
     # scanners_handler.add_scanner(moro_scanner)
-    scanners_handler.add_scanner(moro_big_scanner)
+    # scanners_handler.add_scanner(moro_big_scanner)
 
     # scanners_handler.open("all")
     # scanners_handler.open(moro_scanner)
-    scanners_handler.open(moro_big_scanner)
+    # scanners_handler.open(moro_big_scanner)
 
     # asyncio.run(scanners_handler.ping(moro_scanner))
-    asyncio.run(scanners_handler.ping(moro_big_scanner))
+    # asyncio.run(scanners_handler.ping(moro_big_scanner))
+
+    asyncio.run(scanners_handler.listen())
 
     pass
