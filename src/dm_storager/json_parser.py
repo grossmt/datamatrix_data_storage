898045b993078dadcf978a12ff7cd8366e507159
Emil Bakhabiev <e.bakhabiev@thirdpin.io>
2022-02-21 00:29:42 +0300

    [JSON] WIP: validation schema

--- src/dm_storager/json_parser.py
+++ src/dm_storager/json_parser.py
@@ -6,15 +6,18 @@ from unittest import result
 
 from dm_storager.structs import ScannerStat
 
+
 class ValidationResult(NamedTuple):
     result: bool
     msg: str
 
+
 def is_valid_scanner_info(scanner: ScannerStat) -> ValidationResult:
-    
-    r = ValidationResult()
-    r.result = True
-    r.msg = ""
+
+    r = ValidationResult(result=True, msg="")
+    return r
+    # r.result = True
+    # r.msg = ""
 
 
 def scanners_settings_reading(settings_path: Path) -> List[ScannerStat]:
@@ -22,5 +25,5 @@ def scanners_settings_reading(settings_path: Path) -> List[ScannerStat]:
 
     with open(settings_path, "r") as settings_file:
         scanners = json.load(settings_file)
-        
+
     return scanners
