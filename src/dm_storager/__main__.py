6a343afeca8ff52b0e654a1937602f34f700a2ba
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-03-12 17:18:46 +0300

    [MAIN] Refactored logger and argparse

--- src/dm_storager/__main__.py
+++ src/dm_storager/__main__.py
@@ -1,29 +1,35 @@
-import logging
+import argparse
+from tabnanny import verbose
 
 from dm_storager.server import Server
 from dm_storager.enviroment import HOST_IP, HOST_PORT
-
-logging.basicConfig(
-    level=logging.INFO,
-)
-
-LOGGER = logging.getLogger("Main Programm")
-LOGGER.setLevel(logging.INFO)
-
+from dm_storager.utils.logger import configure_root_logger
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser("Datamatrix Storager")
+
+    parser.add_argument('-v', action="store_true")
+    parser.add_argument('--verbose', action="store_true")
+    parser.set_defaults(v=False)
+    args = parser.parse_args()
+    is_verbose=args.v
 
     server = Server(HOST_IP, HOST_PORT)
     ip, port = server.connection_info
 
-    LOGGER.info(f"Server address: {ip}")
-    LOGGER.info(f"Server port: {port}")
+    main_logger = configure_root_logger("MAIN PROGRAM", is_verbose)
+
+    print("")
+    main_logger.info(f"Server address: {ip}")
+    main_logger.info(f"Server port: {port}")
 
     server.start_server()
 
     try:
         server.run_server()
     except Exception:
-        LOGGER.exception("Server runtime error:")
+        main_logger.exception("Server runtime error:")
         server.stop_server()
+        main_logger.error("Abort program.")
         exit(0)
