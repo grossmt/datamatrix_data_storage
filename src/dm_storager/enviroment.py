85fdc95fa296764b2e2fa4f845f311231b7f892f
Emil Bakhabiev <e.bakhabiev@thirdpin.io>
2022-04-01 09:22:08 +0300

    [ENV] Enchanced local ip resolution

--- src/dm_storager/enviroment.py
+++ src/dm_storager/enviroment.py
@@ -2,7 +2,13 @@ import os
 import socket
 
 hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
+local_ips = socket.gethostbyname_ex(hostname)
+local_ip = "127.0.0.1"
+for ip in local_ips[2]:
+    if "192.1" in ip:
+        local_ip = ip
+        break
 
-HOST_IP = os.getenv('SERVER_IP', local_ip)
-HOST_PORT = os.getenv('SERVER_PORT', 50000)
+
+HOST_IP = os.getenv("SERVER_IP", local_ip)
+HOST_PORT = os.getenv("SERVER_PORT", 50000)
