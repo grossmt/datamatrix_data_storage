6575d9f01392b8270dbc2e6d035af462c786dd99
Emil Bakhabiev <e.bakhabiev@thirdpin.io>
2022-04-08 08:49:23 +0300

    [STRUCTS] Added port type to optional

--- src/dm_storager/structs.py
+++ src/dm_storager/structs.py
@@ -1,23 +1,31 @@
 from dataclasses import dataclass
 from threading import Thread
 from multiprocessing import Process, Queue
+from typing import Optional
 
 
-@dataclass 
+@dataclass
 class ClientMessage:
     client_thread: Thread
     client_ip: str
     client_port: int
     client_message: str
 
+
 @dataclass
 class ScannerInfo:
     address: str
-    port: int
+    port: Optional[int]
     scanner_id: int
-    
+
+
 @dataclass
 class Scanner:
     info: ScannerInfo
     process: Process
     queue: Queue
+
+
+# @dataclass
+# class ClientInfo:
+#     address:
