9a3a98aa519832bc30804d2b1a1a01e19f2220ec
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-04-09 16:29:24 +0300

    [ENV] Moved path of scanner settings to env

--- src/dm_storager/enviroment.py
+++ src/dm_storager/enviroment.py
@@ -1,6 +1,8 @@
 import os
 import socket
 
+from pathlib import Path
+
 hostname = socket.gethostname()
 local_ips = socket.gethostbyname_ex(hostname)
 local_ip = "127.0.0.1"
@@ -12,3 +14,7 @@ for ip in local_ips[2]:
 
 HOST_IP = os.getenv("SERVER_IP", local_ip)
 HOST_PORT = os.getenv("SERVER_PORT", 50000)
+
+SCANNER_SETTINGS = os.getenv(
+    "SCANNER_SETTINGS", Path("settings") / "connection_settings.json"
+)
