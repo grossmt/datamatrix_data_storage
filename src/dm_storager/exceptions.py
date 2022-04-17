4e246f13b4600fc0c02468dfee8b101b5ccd508a
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-04-17 16:59:56 +0300

    [SRC] Added base exception

---
+++ src/dm_storager/exceptions.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import Union
+
+
+class Error(Exception):
+    """Base server exception."""
+
+
+class ConfigNotExists(Error):
+    def __init__(self, config: Path) -> None:
+        super().__init__(
+            'Config file does not exist: "{config}"'.format(config=str(config))
+        )
+
+
+class ScannedIdNotRegistered(Error):
+    def __init__(self, scanner_id: int) -> None:
+        super().__init__(f"Scanner ID#{scanner_id} is not in registered scanner list!")
