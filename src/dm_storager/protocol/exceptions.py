c3cc868099cd40bd2f9b9ca70a45709643bb97a6
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-04-17 22:42:02 +0300

    [PROTOCOL] Removed excessive expressions

--- src/dm_storager/protocol/exceptions.py
+++ src/dm_storager/protocol/exceptions.py
@@ -12,22 +12,8 @@ class TooShortMessage(ProtocolMessageError):
     def __init__(self, slice: bytes) -> None:
         super().__init__(reason="Too short message", msg=slice)
 
-
-class InvalidPreambula(ProtocolMessageError):
-    def __init__(self, slice: bytes) -> None:
-        super().__init__(reason="Invalid Preambula", msg=slice)
-
-
-class InvalidScannerID(ProtocolMessageError):
-    def __init__(self, slice: bytes) -> None:
-        super().__init__(reason="Invalid Scanner ID", msg=slice)
+class InvalidField(ProtocolMessageError):
+    def __init__(self, field: str, slice: bytes) -> None:
+        super().__init__(reason=f"Invalid {field}", msg=slice)
 
 
-class InvalidPacketCode(ProtocolMessageError):
-    def __init__(self, slice: bytes) -> None:
-        super().__init__(reason="Invalid Packet Code", msg=slice)
-
-
-class InvalidPacketID(ProtocolMessageError):
-    def __init__(self, slice: bytes) -> None:
-        super().__init__(reason="Invalid Packet ID", msg=slice)
