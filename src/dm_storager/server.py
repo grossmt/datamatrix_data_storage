e218ec7cca814594104224e04bf6d644a69ac939
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-04-17 17:01:51 +0300

    [SRC] Refactored server

--- src/dm_storager/server.py
+++ src/dm_storager/server.py
@@ -2,12 +2,13 @@ import time
 import multiprocessing
 
 from multiprocessing import Process, Queue
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 from pathlib import Path
-
+from dm_storager.exceptions import ScannedIdNotRegistered
+from dm_storager.protocol.exceptions import ProtocolMessageError
+from dm_storager.protocol.packet_parser import get_scanner_id
 
 from dm_storager.utils.logger import configure_logger
-
 from dm_storager.utils.scanner_network_settings_resolver import (
     resolve_scanners_settings,
 )
@@ -20,21 +21,21 @@ class Server:
     def __init__(self, ip: str, port: int, registred_clients_settings: Path) -> None:
         self._queue = ServerQueue(ip, port)
         self._registred_clients: List[ScannerInfo] = []
-
         self._scanners: List[Scanner] = []
-
         self._settings_path = registred_clients_settings
-
         self._logger = configure_logger("ROOT_SERVER")
 
     @property
-    def connection_info(self) -> Tuple[str, int]:
+    def connection_info(self) -> Optional[Tuple[str, int]]:
         """Provides info about server ip and port.
 
         Returns:
             (str, int): IP-address and port of server.
         """
-        return self._queue.server.server_address
+        if self._queue:
+            return self._queue.server.server_address
+        else:
+            return None
 
     def init_server(self) -> None:
         """Init of server.
@@ -57,7 +58,7 @@ class Server:
 
         for scanner in self._scanners:
             self._logger.warning(
-                f"Scanner #{scanner.info.scanner_id}: killing process."
+                f"Scanner #{scanner.info.scanner_id}: killing process.",
             )
             scanner.process.kill()
 
@@ -76,7 +77,7 @@ class Server:
         is_id_unique: bool = scanner_info.scanner_id in list(
             x.scanner_id for x in self._registred_clients
         )
-        
+
         if not is_id_unique:
             self._registred_clients.append(scanner_info)
             self._logger.info("Registered scanner:")
@@ -101,7 +102,7 @@ class Server:
 
     def is_scanner_alive(self, scanner_id: int) -> bool:
         """Check is scanner alive.
-        
+
         Args:
             scanner_id (int): id of scanner.
 
@@ -117,13 +118,17 @@ class Server:
         for scanner in self._scanners:
             if scanner.info.scanner_id == scanner_id:
                 if scanner.process.is_alive():
-                    self._logger.info(f"Applying new settings to scanner id #{scanner_id}...")
+                    self._logger.info(
+                        f"Applying new settings to scanner id #{scanner_id}..."
+                    )
                 else:
                     self._logger.warning(f"Scanner with id #{scanner_id} is not alive!")
-                    self._logger.warning(f"Settings will be apllied after scanner resurrection!")
+                    self._logger.warning(
+                        f"Settings will be apllied after scanner resurrection!"
+                    )
                 scanner.queue.put(settings)
                 return True
-        
+
         self._logger.error("No scanner with given ID found!")
         return False
 
@@ -135,42 +140,48 @@ class Server:
 
         is_registered = self._is_client_registered(client_message.client_ip)
 
-        if is_registered:
-
-            try:
-                scanner_id_int = int.from_bytes(
-                    client_message.client_message[6:8], byteorder="big"
-                )
-            except Exception:
-                self._logger.error("Bad scanner id")
-                self._logger.error(f"Message: {client_message.client_message}")
-                return
-
-            for scanner in self._scanners:
-                if scanner_id_int == scanner.info.scanner_id:
-                    if scanner.process.is_alive():
-                        scanner.queue.put(client_message.client_message)
-                    else:
-                        scanner.info.port = client_message.client_port
-                        try:
-                            scanner.process.start()
-                        except AssertionError:
-                            self._logger.warning(
-                                f"Process for scanner {scanner.info.name}: ID#{scanner.info.scanner_id} was already started and killed due to error."
-                            )
-                            self._logger.warning("Restarting process...")
-                            scanner.process = Process(
-                                target=scanner_process,
-                                args=(scanner.info, scanner.queue),
-                            )
-                            scanner.process.start()
-
-                    break
-
-        else:
+        if not is_registered:
             self._logger.warning(
                 f"Unregistred scanner connection from {client_message.client_ip}!"
             )
+            return
+
+        try:
+            scanner_id_int = get_scanner_id(client_message.client_message)
+        except ProtocolMessageError as err:
+            self._logger.error(str(err))
+            return
+
+        is_scanner_id_registered: bool = scanner_id_int in list(
+            x.scanner_id for x in self._registred_clients
+        )
+        if not is_scanner_id_registered:
+            self._logger.error(
+                f"Scanner with id {scanner_id_int} on address {client_message.client_ip} is registered, but not found."
+            )
+            self._logger.error("Perhaps bad settings, check it please.")
+            self._logger.error(f"Skipped message: {client_message.client_message}")
+            return
+
+        for scanner in self._scanners:
+            if scanner_id_int == scanner.info.scanner_id:
+                if scanner.process.is_alive():
+                    scanner.queue.put(client_message.client_message)
+                else:
+                    scanner.info.port = client_message.client_port
+                    try:
+                        scanner.process.start()
+                    except AssertionError:
+                        self._logger.warning(
+                            f"Process for scanner {scanner.info.name}: ID#{scanner.info.scanner_id} was already started and killed due to error."
+                        )
+                        self._logger.warning("Restarting process...")
+                        scanner.process = Process(
+                            target=scanner_process,
+                            args=(scanner.info, scanner.queue),
+                        )
+                        scanner.process.start()
+                break  # found given scanner, no need to iterate next
 
     def _is_client_registered(self, client_address: str) -> bool:
         for client in self._registred_clients:
