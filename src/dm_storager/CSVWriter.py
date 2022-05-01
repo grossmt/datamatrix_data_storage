ee67b3661eb2e2dcf1311372a32f0cf07b5a6f50
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-05-01 23:07:10 +0300

    [CSV] Refactored CSV writer

--- src/dm_storager/CSVWriter.py
+++ src/dm_storager/CSVWriter.py
@@ -2,8 +2,13 @@ import csv
 import time
 import datetime
 import os
+
+from csv import reader
+from datetime import date
 from pathlib import Path
 from typing import List, Any
+
+from dm_storager.protocol.schema import ArchieveData, ScannerSettings
 
 appStartTime = datetime.datetime.fromtimestamp(time.time()).strftime(
     "%d-%m-%Y_%H.%M.%S"
@@ -11,45 +16,79 @@ appStartTime = datetime.datetime.fromtimestamp(time.time()).strftime(
 
 
 class CSVWriter(object):
+    
+    HEADER = ["Timestamp", "Product Name", "Record"]
+
     def __init__(self, scanner_id: int):
         self._result_table: List[Any] = []
-        self._filename = self._get_file_path(scanner_id)
+        self._scanner_id = scanner_id
+        self._result_table = []
 
-    def _get_file_path(self, scanner_id: int) -> Path:
+    def append_data(self, archieve_data: ArchieveData, scanner_settings: ScannerSettings) -> None:
 
-        data_dir = Path.cwd() / "saved_data"
-        os.makedirs(data_dir, exist_ok=True)
-        file_path = data_dir / f"scanner_#{scanner_id}.data.csv"
-        open(file_path, "a").close()
+        new_row = []
 
-        return file_path
+        for i in range(archieve_data.records_count):
+            _timestamp = str(datetime.datetime.now())
+            _product_name = scanner_settings.products[
+                archieve_data.product_id
+            ]
+            _record = archieve_data.records[i]
+            new_row = [_timestamp, _product_name, _record]
+            self._result_table.append(new_row)
 
-    def append_data(self, archieve_data: str) -> None:
+        self._store_data()
 
-        count = int(archieve_data[0])
-        record_len = int(archieve_data[1])
-        archieve_data = archieve_data[2:]
-        listed_data = []
+    def _store_header(self) -> None:
+       with open(self._filename, "a", newline="") as csv_file:
+            writer = csv.writer(csv_file, delimiter=";")
+            writer.writerows(CSVWriter.HEADER)
 
-        for i in range(0, count, record_len):
-            record = archieve_data[i : i + record_len]
-            # archieve_data = archieve_data[i + record_len :]
-            listed_data.extend(record)
+    def _store_data(self) -> None:
+        self._filename = self._get_file_path(self._scanner_id)
+        sh = False
+        with open(self._filename, "r") as csv_file:
+            csv_reader = reader(csv_file)
+            try:
+                header = next(csv_reader)
+            except StopIteration:
+                sh = True
+        if sh:
+            with open(self._filename, "a", newline="") as csv_file:
+                writer = csv.writer(csv_file, delimiter=";")
+                writer.writerow(CSVWriter.HEADER)
 
-        self._result_table.append(listed_data)
-
-    def store_data(self) -> None:
-
-        with open(self._filename, "w", newline="") as csv_file:
+        with open(self._filename, "a", newline="") as csv_file:
             writer = csv.writer(csv_file, delimiter=";")
             writer.writerows(self._result_table)
 
+    def _get_file_path(self, scanner_id: int) -> Path:
+        data_dir = Path.cwd() / "saved_data"
+        os.makedirs(data_dir, exist_ok=True)
+        current_date = str(date.today())
+        file_path = data_dir / f"scanner_#{scanner_id}.{current_date}.csv"
+        open(file_path, "a").close()
+        return file_path
 
-test_id = 1
-tl1 = [5, 1, 1, 2, 3, 4, 5]
-tl2 = [3, 1, "a", "b", "c"]
-wr = CSVWriter(test_id)
-wr.append_data(tl1)
-wr.store_data()
-wr.append_data(tl2)
-wr.store_data()
+# w = CSVWriter(0)
+# s_set = ScannerSettings(
+#     products=["Product1","Product2","Product3","","","",],
+#     server_ip="",
+#     server_port=0,
+#     gateway_ip="",
+#     netmask="",
+#     reserved=0
+# )
+
+# a_data = ArchieveData(
+#     records_count=2,
+#     record_len=8,
+#     product_id=1,
+#     records=[
+#         "RECORD-1",
+#         "RECORD-2",
+#     ]
+# )
+
+# w.append_data(a_data, s_set)
+# pass
