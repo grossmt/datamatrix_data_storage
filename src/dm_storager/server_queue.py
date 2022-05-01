8619597685d39a6ff4b3e718c58745b7f0b25065
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-05-01 23:06:50 +0300

    [SERVER] Added new connection handling

--- src/dm_storager/server_queue.py
+++ src/dm_storager/server_queue.py
@@ -1,9 +1,10 @@
 import threading
 import socketserver
+from socket import timeout
 
 from typing import List, Tuple
 
-from dm_storager.structs import ClientMessage
+from dm_storager.structs import ClientMessage, HandshakeMessage
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
@@ -11,31 +12,62 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-    def handle(self):  # noqa: WPS110
-        while True:
-            b_data = self.request.recv(4096)
+    def setup(self):
+        self.request.settimeout(1.0)
+        # self._is_runnin
 
+    def handle(self):  # noqa: WPS110
+
+        is_first_handshake: bool = True
+
+        while self.server._is_running:
             cur_thread = threading.current_thread()
             client_ip = self.client_address[0]
             client_port = self.client_address[1]
 
-            _message = ClientMessage(
-                client_thread=cur_thread,
-                client_ip=client_ip,
-                client_port=int(client_port),
-                client_message=b_data
-            )
+            b_data: bytes = b''
+            try:
+                b_data = self.request.recv(1024)
+            except timeout as ex:
+                if is_first_handshake:
+                    # self.server._logger.info(f"Got connection from {self.client_address[0]}")
+                    _message = HandshakeMessage(
+                        client_socket=self.request,
+                        client_ip=self.client_address[0],
+                        client_port=self.client_address[1],
+                    )
+                    is_first_handshake = False
+                    self.server.queue.add(_message)
 
-            self.server.queue.add(_message)
+                continue
+            except Exception:
+                b_data = b''
 
+            if b_data:
+                _message = ClientMessage(
+                    client_thread=cur_thread,
+                    client_ip=client_ip,
+                    client_port=int(client_port),
+                    client_message=b_data,
+                )
+                self.server.queue.add(_message)
+            else:
+                break
 
+        self.request.close()
+        self.server._logger.warning(f"Connection from {self.client_address[0]} closed.")
+        
+    
 class ServerQueue:
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, logger):
         self.server = ThreadedTCPServer((ip, port), ThreadedTCPRequestHandler)
         self.server.queue = self
         self.server_thread = threading.Thread(target=self.server.serve_forever)
-        self.server_thread.daemon = True
+        self.server_thread.daemon = False
+        self.server.block_on_close = True
         self.messages: List[ClientMessage] = []
+        self.server._is_running = True
+        self.server._logger = logger
 
     @property
     def connection_info(self) -> Tuple[str, int]:
@@ -45,8 +77,7 @@ class ServerQueue:
         self.server_thread.start()
 
     def stop_server(self):
-        self.server.shutdown()
-        self.server.server_close()
+        pass
 
     def add(self, message):
         self.messages.append(message)
