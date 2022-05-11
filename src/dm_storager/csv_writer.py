1104db8a08f4fe337edfdb6394b753f20e2a7fdf
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-05-12 01:17:48 +0300

    [CSV] Formatted csv writer

--- src/dm_storager/csv_writer.py
+++ src/dm_storager/csv_writer.py
@@ -16,7 +16,7 @@ appStartTime = datetime.datetime.fromtimestamp(time.time()).strftime(
 
 
 class CSVWriter(object):
-    
+
     HEADER = ["Timestamp", "Product Name", "Record"]
 
     def __init__(self, scanner_id: int):
@@ -24,15 +24,15 @@ class CSVWriter(object):
         self._scanner_id = scanner_id
         self._result_table = []
 
-    def append_data(self, archieve_data: ArchieveData, scanner_settings: ScannerSettings) -> None:
+    def append_data(
+        self, archieve_data: ArchieveData, scanner_settings: ScannerSettings
+    ) -> None:
 
         new_row = []
 
         for i in range(archieve_data.records_count):
             _timestamp = str(datetime.datetime.now())
-            _product_name = scanner_settings.products[
-                archieve_data.product_id
-            ]
+            _product_name = scanner_settings.products[archieve_data.product_id]
             _record = archieve_data.records[i]
             new_row = [_timestamp, _product_name, _record]
             self._result_table.append(new_row)
@@ -40,7 +40,7 @@ class CSVWriter(object):
         self._store_data()
 
     def _store_header(self) -> None:
-       with open(self._filename, "a", newline="") as csv_file:
+        with open(self._filename, "a", newline="") as csv_file:
             writer = csv.writer(csv_file, delimiter=";")
             writer.writerows(CSVWriter.HEADER)
 
@@ -69,26 +69,3 @@ class CSVWriter(object):
         file_path = data_dir / f"scanner_#{scanner_id}.{current_date}.csv"
         open(file_path, "a").close()
         return file_path
-
-# w = CSVWriter(0)
-# s_set = ScannerSettings(
-#     products=["Product1","Product2","Product3","","","",],
-#     server_ip="",
-#     server_port=0,
-#     gateway_ip="",
-#     netmask="",
-#     reserved=0
-# )
-
-# a_data = ArchieveData(
-#     records_count=2,
-#     record_len=8,
-#     product_id=1,
-#     records=[
-#         "RECORD-1",
-#         "RECORD-2",
-#     ]
-# )
-
-# w.append_data(a_data, s_set)
-# pass
