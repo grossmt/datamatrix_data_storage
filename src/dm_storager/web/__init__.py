7c6436f6b66f3fc72f7fd04f3c7a6abe34a5a091
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-06-29 01:13:45 +0300

    [WEB] Added log page

---
+++ src/dm_storager/web/__init__.py
@@ -0,0 +1,3 @@
+from dm_storager.web.app import run_app as run_web_server
+
+__all__ = ["run_web_server"]
---
+++ src/dm_storager/web/app.py
@@ -0,0 +1,31 @@
+import time
+import datetime
+import logging
+
+from flask import Flask, render_template
+from flask import Response
+
+from dm_storager.web.logger import flask_logger
+
+APP = Flask(__name__, static_folder="app/static/", template_folder="app/static/")
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.CRITICAL)
+
+
+@APP.route("/", methods=["GET"])
+def root():
+    """index page"""
+    return render_template("index.html")
+
+
+@APP.route("/log_stream", methods=["GET"])
+def log_stream():
+    """returns logging information"""
+    return Response(
+        flask_logger(), mimetype="text/plain", content_type="text/event-stream"
+    )
+
+
+def run_app(host: str = "127.0.0.1", port: int = 80):
+    APP.run(host, port, threaded=True, debug=False)
