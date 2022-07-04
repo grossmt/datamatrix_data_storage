7ca5a3a58940e5ee1309882db735b255512aa7a0
E. Bakhabiev <e.bakhabiev@thirdpin.io>
2022-07-05 00:03:08 +0300

    [WEB] Some shit with script running

--- src/dm_storager/web/static/js/log.js
+++ src/dm_storager/web/static/js/log.js
@@ -1,35 +1,21 @@
 $(document).ready(
-    update_log(),
-    setInterval(update_log, 1000)
+
 );
-
-
-// $(function () {
-//     update_log();
-//     setInterval(update_log, 1000);
-// });
 
 function update_log() {
     var output = document.getElementById('output');
     var requester = new XMLHttpRequest();
 
-    // requester.open('GET', '{{ url_for('log_stream') }}', true);
-    requester.open(
-        'GET',
-        '{{ url_for('log_stream') }}',
-        true
-    );
+    requester.open('GET', '{{ url_for('log_stream') }}', true);
     requester.send();
 
     setTimeout(function () {
-
-        if ($(requester.responseText).text() != output.text()) {
-            output.html("");
+        if ($(requester.responseText).text() != $('#output').text()) {
+            $('#output').html("");
             output.insertAdjacentHTML("afterbegin", requester.responseText);
-        }
-        else {
-            console.log("no new loigs")
         }
     }, 1000);
 }
 
+update_log();
+setInterval(update_log, 1000);
